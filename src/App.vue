717eaaa73e30f63b151da601120906bed1600256
dddw001 <2410656679@qq.com>
2019-09-17 10:48:37 +0800

    scrollbar style

--- src/App.vue
+++ src/App.vue
@@ -32,3 +32,33 @@ body {
 	margin: 0px;
 }
 </style>
+<style lang="stylus">
+body::-webkit-scrollbar {
+	/* 滚动条整体样式 */
+	width: 10px; /* 高宽分别对应横竖滚动条的尺寸 */
+	height: 1px;
+}
+
+body::-webkit-scrollbar-thumb {
+	/* 滚动条里面小方块 */
+	border-radius: 10px;
+	background-color: skyblue;
+	background-image: -webkit-linear-gradient(
+		45deg,
+		rgba(255, 255, 255, 0.2) 25%,
+		transparent 25%,
+		transparent 50%,
+		rgba(255, 255, 255, 0.2) 50%,
+		rgba(255, 255, 255, 0.2) 75%,
+		transparent 75%,
+		transparent
+	);
+}
+
+body::-webkit-scrollbar-track {
+	/* 滚动条里面轨道 */
+	box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+	background: #ededed;
+	border-radius: 10px;
+}
+</style>
